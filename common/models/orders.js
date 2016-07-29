47daf11f9b0e0ad640dcbc73e9c9e033677dba4e
WesleyMcAndrews <wesley@mcandrews.com.br>
2016-07-29 13:46:21 -0300

    Add passport support

--- common/models/orders.js
+++ common/models/orders.js
@@ -1,3 +1,9 @@
 module.exports = function(Orders) {
 
+  //Impedir que um usuário crie pedidos para outro
+  Orders.beforeRemote('create', function(ctx, modelInstance, next) {
+    ctx.args.data.userId = ctx.req.accessToken.userId;
+    next();
+  });
+  
 };
